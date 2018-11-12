e3f8c9ba9b215ec6061d4aa09c717bbcd6abb8c3
Huffman-Kristopher <nokins_@hotmail.com>
2018-11-12 09:38:38 -0800

    update serverlet

---
+++ serverlet/WebApplication1/src/java/HelloForm.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kristopher Huffman
+ */
+
+   // Extend HttpServlet class
+public class HelloForm extends HttpServlet {
+ 
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set response content type
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      String title = "Using GET Method to Read Form Data";
+      String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+         
+      out.println(docType +
+         "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+               "<h1 align = \"center\">" + title + "</h1>\n" +
+               "<ul>\n" +
+                  "  <li><b>First Name</b>: "
+                  + request.getParameter("first_name") + "\n" +
+                  "  <li><b>Last Name</b>: "
+                  + request.getParameter("last_name") + "\n" +
+               "</ul>\n" +
+            "</body>" +
+         "</html>"
+      );
+   }
+}
