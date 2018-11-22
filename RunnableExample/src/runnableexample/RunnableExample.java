4a3eeb713ee400f2bddf14fecdd5e10600e404f1
Huffman-Kristopher <nokins_@hotmail.com>
2018-11-22 15:20:38 -0800

    Runnables threads executors

---
+++ RunnableExample/src/runnableexample/RunnableExample.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runnableexample;
+
+/**
+ *
+ * @author Kristopher Huffman
+ */
+public class RunnableExample implements Runnable {
+
+    public static void main(String[] args) {
+        System.out.println("Inside : " + Thread.currentThread().getName());
+
+        System.out.println("Creating Runnable...");
+        Runnable runnable = new RunnableExample();
+
+        System.out.println("Creating Thread...");
+        Thread thread = new Thread(runnable);
+
+        System.out.println("Starting Thread...");
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inside : " + Thread.currentThread().getName());
+    }
+}
